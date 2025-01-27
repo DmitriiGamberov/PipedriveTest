1c46dcdc6f22283214bb77a8603e29fbcfdc8223
Dmitrii Gamberov <moujk94@gmail.com>
2025-01-27 19:10:20 +0200

    Init

---
+++ PipedriveTest/PipedriveTest/PipedriveTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PipedriveTestApp.swift
+//  PipedriveTest
+//
+//  Created by Dmitrii Gamberov on 27.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct PipedriveTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
